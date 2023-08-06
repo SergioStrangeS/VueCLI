cff314defd59a3e581b17b0cc1b8c890d3109775
Sergio Stranger <xabarov-05@mail.ru>
2023-08-06 18:45:07 +0300

    First commit

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <h1>Vue TodoList</h1>
+  <TodoList :todos="todos" @delete-todo="DeleteTodo"/>
+</template>
+
+<script>
+import TodoList from './components/TodoList.vue';
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      todos: ['Hello', 'World', 'Vue']
+    }
+  },
+  components: {
+    TodoList
+  },
+  methods: {
+    DeleteTodo(index) {
+      this.todos.splice(index, 1)
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,24 @@
+<template>
+  <ul>
+    <li v-for="(todo, index) in todos" :key="todo.id" @click="deleteTodo(index)">{{index+1}}. {{todo}}</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: ['todos'],
+  methods: {
+    deleteTodo(index) {
+      this.$emit('delete-todo', index)
+    }
+  }
+}
+</script>
+
+<style scoped>
+  ul {
+    list-style: none;
+    padding: 0;
+    cursor: pointer;
+  }
+</style>
